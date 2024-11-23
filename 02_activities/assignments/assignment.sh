9892757694e2e92768d7178b7077fc0a6da392b1
Jasonpereira0 <jason.pere@gmail.com>
2024-11-23 12:48:06 -0500

    edited 8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,8 +47,7 @@ rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd data/processed
-ls >> data/inventory.txt
+ls /data/processed >> data/inventory.txt
 
 
 ###########################################
